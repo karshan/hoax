cd5e2b18c63ca6436a7eb8d42489b5022ce787d3
karshan <karshan.sharma@gmail.com>
2018-01-01 19:39:39 -0800

    use bracket to cleanup sockets on exceptions

--- Main.hs
+++ Main.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 import           Control.Applicative              ((<$>), (<|>))
-import           Control.Concurrent               (forkIO)
-import           Control.Exception                (SomeException, catch, try)
+import           Control.Concurrent.Async         (async, wait, waitBoth)
+import           Control.Exception                (SomeException, bracket,
+                                                   catch, try)
 import           Control.Monad                    (forever)
 import           Data.Attoparsec.ByteString       (eitherResult, maybeResult,
                                                    parse, parseOnly)
-import           Data.Attoparsec.ByteString.Char8 hiding (eitherResult,
-                                                   maybeResult, parse,
-                                                   parseOnly, try)
+import           Data.Attoparsec.ByteString.Char8 (Parser)
+import qualified Data.Attoparsec.ByteString.Char8 as P
 import           Data.ByteString                  (ByteString)
 import qualified Data.ByteString                  as B
 import qualified Data.ByteString.UTF8             as B (toString)
@@ -20,17 +23,16 @@ import           Data.String                      (IsString (..))
 import           GHC.Word                         (Word8)
 import           Network.Socket                   hiding (recv, recvFrom, send,
                                                    sendTo)
-import           Network.Socket.ByteString
-import           Network.Socks5                   ()
-import           Prelude                          hiding (getContents, takeWhile)
+import           Network.Socket.ByteString        (recv, recvFrom, send,
+                                                   sendAll, sendTo)
+import           Prelude                          (String)
+import           Protolude
 import           System.Environment               (getArgs)
 import           System.Exit                      (exitSuccess)
 import           System.Posix.Signals             (Handler (Catch),
                                                    installHandler,
                                                    keyboardSignal)
 import           System.Random                    (randomIO)
-
--- TODO handle exceptions
 
 instance IsString [Word8] where
     fromString = map (fromIntegral . ord)
@@ -42,46 +44,68 @@ data HTTPRequest = HTTPRequest {
         qBody    :: B.ByteString
     } deriving (Show, Read, Eq, Ord)
 
-setPort :: PortNumber -> SockAddr -> SockAddr
-setPort p (SockAddrInet _ h) = SockAddrInet p h
-setPort p (SockAddrInet6 _ f h s) = SockAddrInet6 p f h s
-setPort _ _ = error "wtf seriously, getaddrinfo returned a unix socket !?"
+setPort :: PortNumber -> SockAddr -> Either Text SockAddr
+setPort p (SockAddrInet _ h) = Right $ SockAddrInet p h
+setPort p (SockAddrInet6 _ f h s) = Right $ SockAddrInet6 p f h s
+setPort _ _ = Left "wtf seriously, getaddrinfo returned a unix socket !?"
 
-connect' :: ByteString -> Integer -> IO (Maybe Socket)
-connect' host port = do
-    out_socket <- socket AF_INET Stream defaultProtocol
-    msockaddr <- fmap (setPort (fromInteger port) . addrAddress) . listToMaybe . filter ((/= AF_INET6) . addrFamily) <$>
-        getAddrInfo Nothing (Just . B.toString $ host) Nothing
-    msockaddr & maybe (putStrLn "getAddrInfo failed" >> return Nothing) (\sockaddr -> do
-        e <- try (connect out_socket sockaddr)
-        either (\(_ :: SomeException) -> putStrLn "connect failed" >> return Nothing) (const (return (Just out_socket))) e)
+withSocket ::  Family -> SocketType -> ProtocolNumber -> (Socket -> IO a) -> IO a
+withSocket family sktType protocol =
+    bracket (socket family sktType protocol)  -- acquire resource
+            (\skt -> shutdown skt ShutdownBoth >> close skt) -- release
 
-errorPrint :: (Show a) => String -> Maybe a -> IO ()
-errorPrint msg m_x = putStrLn $ "ERROR: " ++ msg ++ maybe "" (\x -> "<" ++ show x ++ ">") m_x
+monadBind :: Monad m => (a -> m b) -> m a -> m b
+monadBind = (=<<)
 
-debug :: (Show a) => String -> Maybe a -> IO ()
--- debug = const $ return ()
-debug msg m_x = putStrLn $ "DEBUG: " ++ msg ++ maybe "" (\x -> "<" ++ show x ++ ">") m_x
+-- TODO return EitherT IO instead ?
+getAddrInfo' :: StringConv host String => host -> Integer -> IO (Either Text SockAddr)
+getAddrInfo' host port =
+    (setPort (fromInteger port) . addrAddress <=< maybeToEither "getAddrInfo no results" . head) .
+    filter ((/= AF_INET6) . addrFamily) <$>
+        getAddrInfo Nothing (Just . toS $ host) Nothing
 
+withConnect :: ByteString -> Integer -> (Socket -> IO a) -> IO (Either Text a)
+withConnect host port cont =
+    withSocket AF_INET Stream defaultProtocol $ \s -> do
+        getAddrInfo' host port >>=
+            either
+                (return . Left)
+                (\sockaddr -> do
+                    connect s sockaddr
+                    fmap Right $ cont s)
+
+logPrint :: (Show a) => Text -> Text -> Maybe a -> IO ()
+logPrint prefix msg m_x = putStrLn $ prefix <> ": " <> msg <> maybe "" (\x -> "<" <> show x <> ">") m_x
+
+debug :: (Show a) => Text -> Maybe a -> IO ()
+debug = logPrint "debug"
+
+err :: (Show a) => Text -> Maybe a -> IO ()
+err = logPrint "error"
+
+-- TODO OptParse Applicative stack template
 main :: IO ()
 main = do
-    port <- (fromInteger . fromMaybe 3000 . fmap read . listToMaybe) <$> getArgs
+    port <- (fromInteger . fromMaybe 3000 . monadBind readMaybe . head) <$> getArgs
     addr <- inet_addr "0.0.0.0"
-    s <- socket AF_INET Stream defaultProtocol
-    setSocketOption s ReuseAddr 1
-    installHandler keyboardSignal (Catch $ close s >> exitSuccess) Nothing
-    bind s $ SockAddrInet port addr
-    listen s 10
-    putStrLn $ "Listening on port " ++ show port
-    forever $ do
-        (c, remote) <- accept s
-        forkIO $ worker c remote
+    withSocket AF_INET Stream defaultProtocol $ \s -> do
+        setSocketOption s ReuseAddr 1
+        installHandler keyboardSignal (Catch $ close s >> exitSuccess) Nothing
+        bind s $ SockAddrInet port addr
+        listen s 10
+        debug "Listening on port " (Just port)
+        forever $ do
+            (c, remote) <- accept s
+            async (worker c remote)
 
 worker :: Socket -> SockAddr -> IO ()
 worker c remote = do
-    reqraw <- recv c (32*1024)
-    either (\x -> errorPrint "parseOnly httpRequest" (Just (x, reqraw, remote))) (\req -> do
-        forward req c) $ parseOnly httpRequest reqraw
+    bracket_ (return ())
+        (shutdown c ShutdownBoth >> close c)
+        (do
+            reqraw <- recv c (32*1024)
+            either (\x -> err "parseOnly httpRequest" (Just (x, reqraw, remote))) (\req -> do
+                forward req c) $ parseOnly httpRequest reqraw)
 
 changePort (SockAddrInet _ h) p      = SockAddrInet p h
 changePort (SockAddrInet6 _ a b c) p = SockAddrInet6 p a b c
@@ -90,26 +114,28 @@ changePort (SockAddrInet6 _ a b c) p = SockAddrInet6 p a b c
 
 forward :: HTTPRequest -> Socket -> IO ()
 forward req@(HTTPRequest { qMethod = "CONNECT" }) c = do
-    either (errorPrint "parseOnly connectHost" . Just) (\(h, p) -> do
+    either (err "parseOnly connectHost" . Just) (\(h, p) -> do
         debug "CONNECT to" (Just (h, p))
-        mSock <- connect' h (read (B.toString p)) -- this read won't fail because p is guaranteed to be a string containing only digits by the connectHost parser
-        maybe (close c >> errorPrint "connect failed" (Just req))
-            (\sock -> do
-                sendAll c connect200response
-                _ <- forkIO $ doCopy c sock
-                _ <- forkIO $ doCopy sock c
-                return ())
-            mSock)
+        maybe
+            (err "failed to read port" (Just p))
+            (\portInt -> do
+                void $ withConnect h portInt $ \s -> do
+                    sendAll c connect200response
+                    a1 <- async $ doCopy c s
+                    a2 <- async $ doCopy s c
+                    waitBoth a1 a2
+                    return ())
+            (readMaybe (toS p)))
         (parseOnly connectHost (qPath req))
         where
             connect200response = "HTTP/1.1 200 OK\r\nConnection Established\r\n\r\n"
-forward req c = errorPrint "not-CONNECT" (Just (qMethod req, qPath req)) >> close c
+forward req c = err "not-CONNECT" (Just (qMethod req, qPath req))
 
 doCopy :: Socket -> Socket -> IO ()
 doCopy from to = do
-    r <- (recv from (32*1024)) `catch` (\(e :: SomeException) -> close from >> return "") -- Magic Numbers from golang
-    if B.null r then close from
-    else (sendAll to r >> doCopy from to) `catch` (\(e :: SomeException) -> close from)
+    r <- (recv from (32*1024)) -- Magic Numbers from golang
+    if B.null r then return ()
+    else (sendAll to r >> doCopy from to)
 
 {- TODO helpers
     skipSpace' = skipWhile (== ' ')
@@ -118,41 +144,41 @@ doCopy from to = do
 
 connectHost :: Parser (B.ByteString, B.ByteString)
 connectHost = do
-    h <- takeWhile (/= ':')
-    char ':'
-    p <- takeWhile isDigit
+    h <- P.takeWhile (/= ':')
+    P.char ':'
+    p <- P.takeWhile isDigit
     return (h, p)
         where
             isDigit w = w >= '0' && w <= '9'
 
 method :: Parser B.ByteString
-method = choice $ map stringCI [ "CONNECT", "OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE" ]
+method = P.choice $ map P.stringCI [ "CONNECT", "OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE" ]
 
 uri :: Parser B.ByteString
-uri = takeWhile (not . isSpace) -- TODO actually only ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;= should be valid (RFC 3986) so we want an efficient Char -> Bool that is True for these chars
+uri = P.takeWhile (not . P.isSpace) -- TODO actually only ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;= should be valid (RFC 3986) so we want an efficient Char -> Bool that is True for these chars
 
 httpVersion :: Parser B.ByteString
-httpVersion = string "HTTP/1.1" <|> string "HTTP/1.0"
+httpVersion = P.string "HTTP/1.1" <|> P.string "HTTP/1.0"
 
 -- Note this will miss headers that do not end with \r\n.
 header :: Parser (B.ByteString, B.ByteString)
 header = do
-    k <- takeWhile (\x -> not (isSpace x) && x /= ':')
-    char ':'
-    skipWhile (== ' ')
-    v <- takeWhile (\x -> x /= '\r' && x /= '\n') -- this is not precise I want to takeWhile 2 bytes aren't equal to "\r\n" TODO
-    string "\r\n" <|> string "\n"
+    k <- P.takeWhile (\x -> not (P.isSpace x) && x /= ':')
+    P.char ':'
+    P.skipWhile (== ' ')
+    v <- P.takeWhile (\x -> x /= '\r' && x /= '\n') -- this is not precise I want to takeWhile 2 bytes aren't equal to "\r\n" TODO
+    P.string "\r\n" <|> P.string "\n"
     return (k, v)
 
 httpRequest :: Parser HTTPRequest
 httpRequest = do
     m <- method
-    skipWhile (== ' ')
+    P.skipWhile (== ' ')
     u <- uri
-    skipWhile (== ' ')
+    P.skipWhile (== ' ')
     _ <- httpVersion
-    string "\r\n" <|> string "\n"
-    hs <- many' header
-    string "\r\n" <|> string "\n"
-    b <- takeByteString -- TODO takeLazyByteString ???
+    P.string "\r\n" <|> P.string "\n"
+    hs <- P.many' header
+    P.string "\r\n" <|> P.string "\n"
+    b <- P.takeByteString -- TODO takeLazyByteString ???
     return $ HTTPRequest m u hs b
